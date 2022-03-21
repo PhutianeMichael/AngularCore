67b93b66720bf27467a2a18de1000d2d336fd739
michael.r <michael.r@myquest.co>
2022-03-21 18:39:39 +0200

    Angular Attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,33 @@
-import { Component } from '@angular/core';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { COURSES } from './db-data';
+import { HighlightedDirective } from './directives/highlighted.directive';
 import { Course } from './models/course';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  courses:Course[] = COURSES;
-  title:string = "Angular Core"
-  course:Course;
+export class AppComponent implements AfterViewInit {
+  courses: Course[] = COURSES;
+  title: string = 'Angular Core';
+  course: Course;
 
-  constructor() {
+  @ViewChild(CourseCardComponent, {read: HighlightedDirective})
+  highlightedDirective: HighlightedDirective;
 
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    console.log(this.highlightedDirective);
   }
 
-
-
-
-  onCourseSelected(course:Course){
+  onCourseSelected(course: Course) {
     this.course = course;
   }
 
+  onToggle(event: any) {
+    console.log(event);
+  }
 }
