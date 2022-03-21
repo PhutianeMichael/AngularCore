23e108c2b60aed56c38d9802f8984299c3ba5441
michael.r <michael.r@myquest.co>
2022-03-21 19:35:23 +0200

    custom angular stractural directive

--- src/app/course-image/course-image.component.ts
+++ src/app/course-image/course-image.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-course-image',
+  selector: 'course-image',
   templateUrl: './course-image.component.html',
   styleUrls: ['./course-image.component.css']
 })
